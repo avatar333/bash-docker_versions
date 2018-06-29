38bc3d29d1a3e4ac240bd44d3d8975129db23ef2
Kevinesan Pillay <kevin.pillay@is.co.za>
2018-06-29 11:21:57 +0200

    Version 1.0

--- docker_versions.sh
+++ docker_versions.sh
@@ -1,29 +1,29 @@
 #!/usr/bin/env bash
 #title           :docker_versions.sh
 #description     :A script to display running and available version for each docker
-#author		 :Kevin Pillay
+#author          :Kevin Pillay
 #date            :20180629
 #version         :0.1
-#usage		 :bash docker_versions.sh
+#usage           :bash docker_versions.sh
 #notes           :
 #bash_version    :GNU bash, version 4.4.19(1)-release (x86_64-redhat-linux-gnu)
 #==============================================================================
 
 if [[ $(id -u $USERNAME) -ne 0 ]]
 then
-	printf "\nYou need to run this as root\n"
-	exit 1
+        printf "\nYou need to run this as root\n"
+        exit 1
 fi
 
-for DOCKER in $(docker ps -a | grep -v IMAGE| awk '{print $2}')
+for DOCKER in $(docker ps -a | grep -v IMAGE| awk -F '[[:space:]][[:space:]]+' '{print $6}')
 do
-	IMAGE=$(docker ps -a | grep $DOCKER | awk '{print $2}')
-	NAME=$(docker ps -a | grep $DOCKER | awk '{print $10}')
+        IMAGE=$(docker ps -a | grep $DOCKER | awk -F '[[:space:]][[:space:]]+' '{print $2}')
+        NAME=$(docker ps -a | grep $DOCKER | awk -F '[[:space:]][[:space:]]+' '{print $6}')
 
-	printf "Image version for : $DOCKER\n"
-	printf "docker inspect -f '{{ index .Config.Labels "build_version" }}' $IMAGE\n"
+        printf "Image version for : $DOCKER\n"
+        printf "docker inspect -f '{{ index .Config.Labels \"build_version\" }}' $IMAGE\n" | /bin/bash
 
-	printf "Container version for : $DOCKER\n"
-	printf "docker inspect -f '{{ index .Config.Labels "build_version" }}' $NAME\n\n"
+        printf "Container version for : $DOCKER\n"
+        printf "docker inspect -f '{{ index .Config.Labels \"build_version\" }}' $NAME\n\n" | /bin/bash
 
 done
